84356526d5033260f39db982033160227135599d
Milton-Pires <mpnn2207@gmail.com>
2025-11-13 19:58:07 -0300

    Java login feito

--- app/src/main/java/br/com/etecia/biscoiteapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/biscoiteapp/LoginActivity.java
@@ -1,6 +1,10 @@
 package br.com.etecia.biscoiteapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +12,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
+public class LoginActivity extends AppCompatActivity {
+    MaterialButton btnEntrar, btnCadastro;
+    TextView txtEsqueceu;
+    TextInputEditText edtEmail, edtSenha;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +28,40 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnCadastro = findViewById(R.id.btnCadastro);
+        txtEsqueceu = findViewById(R.id.txtEsqueceu);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtSenha = findViewById(R.id.edtSenha);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = edtEmail.getText().toString();
+                String senha = edtSenha.getText().toString();
+
+                if(email.equals("etecia") && senha.equals("12345")){
+                    startActivity(new Intent(getApplicationContext(), MenuPrincipalActivity.class));
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Usuario ou senha incorretos!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), CadastrarUsuarioActivity.class));
+                finish();
+            }
+        });
+
+        txtEsqueceu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), RecuperarSenhaActivity.class));
+            }
         });
     }
 }
